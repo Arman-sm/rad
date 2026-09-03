d224704c8587b661f3adf1bce8cff801db723ee7
Arman-sm <arman.s.m@outlook.com>
2026-09-03 12:16:25 +0330

    Removed host playback capability

--- rad_host_playback/src/lib.rs
+++
@@ -1,6 +0,0 @@
-// This crate is dedicated to the adapter for playing the audio on the host system but is currently disabled.
-
-// pub mod playback;
-// mod writer;
-
-// pub use playback::init_host_playback_default;
